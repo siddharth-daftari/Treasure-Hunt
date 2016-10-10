d6b0e59ae19c250f4d5467b5fe4d835031fb5147
Gagan Jain <gagan.jain@sjsu.edu>
2016-10-09 23:42:22 -0700

    Update Island.java

--- TreausreHunt/Island.java
+++ TreausreHunt/Island.java
@@ -3,11 +3,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Island here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Gagan Jain) 
+ * @version (10/9/2016)
  */
 public class Island extends Actor
 {
+    private boolean hasShip;
+    private Island nextIslandA;
+    private Island nextIslandB;
     
     public void act() 
     {
@@ -15,4 +18,29 @@ public class Island extends Actor
     }
     
     
+    public boolean getHasShip(){ 
+        return this.hasShip;
+    }
+    
+    public void setHasShip(boolean hasShip){
+        this.hasShip = hasShip;
+    }
+    
+    
+    public Island getNextIslandA(){
+        return this.nextIslandA;
+    }
+    
+    public void setNextIslandA(Island nextIslandA){
+        this.nextIslandA = nextIslandA;
+    }
+    
+    
+    public Island getNextIslandB(){
+        return this.nextIslandB;
+    }
+    
+    public void setNextIslandB(Island nextIslandB){
+        this.nextIslandB = nextIslandB;
+    }
 }
