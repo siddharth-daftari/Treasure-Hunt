c66d06cdbaa8314c1df76d3200a914f357f691be
Gagan Jain <gagan.jain@sjsu.edu>
2016-11-06 23:01:53 -0800

    Update TestClientPost.java
    Updated TestClientPost file to test client code

--- treasure hunt server/TreasureHuntServer/src/TestClientPost.java
+++ treasure hunt server/TreasureHuntServer/src/TestClientPost.java
@@ -19,7 +19,38 @@ public class TestClientPost {
 	
 	public static void main( String[] args ) 
 	{
+		String myURL = "http://localhost:8080/treasureHunt/get";
+		ClientResource client = new ClientResource( myURL ); 
+		String result = "" ;
+		
+		client.setOnResponse(new Uniform() {
+		    public void handle(Request request, Response response) {
+		    	JSONObject json_response = null;
+				try {
+					json_response = new JSONObject( response.getEntity().getText() );
+				} catch (JSONException | IOException e) {
+					e.printStackTrace();
+				}
+		        System.out.println("---------> " + json_response.toString());
 		        
+		        Map<String, Object> map1 = new HashMap<String, Object>();
+
+				// convert JSON string to Map
+		        ObjectMapper mapper = new ObjectMapper();
+				try {
+					map1 = mapper.readValue(json_response.toString(), new TypeReference<Map<String, String>>(){});
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				System.out.println(map1.get("sid"));
+		    }
+
+		});
+		// get scores
+        Representation scores = client.get() ; 
+        
         
 	}
 }
