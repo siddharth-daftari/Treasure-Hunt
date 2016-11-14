b198310ffb0a89a1c29102337e90060aeec30a6e
siddharth.daftari <siddharth.daftari@gmail.com>
2016-11-13 21:13:26 -0800

    code for time left on server side

--- treasure hunt server/TreasureHuntServer/src/treasureHunt/TreasureHunt.java
+++ treasure hunt server/TreasureHuntServer/src/treasureHunt/TreasureHunt.java
@@ -2,24 +2,48 @@ package treasureHunt;
 
 import java.util.*;
 
+
 public class TreasureHunt {
 	private static TreasureHunt treasureHunt ;
 	private Map<String,Object> map = new HashMap<String, Object>();
-	
+	private ArrayList winnerList = new ArrayList(); 
+	public static int timeLeft=0;
+	public TreasureHunt() {
+		
+		treasureHunt = this;
+		
+		Timer timer = new Timer();
+		map.put("timeup", "false");
+		TimerTask task = new TimerTask()
+		{
+		
+		    @Override
+		    public void run()
+		    {
+		    	timeLeft++;
+		    	//System.out.println("time is: " + timeLeft);
+			    
+		    	if(timeLeft == 40)
+		    	{
+		    	    System.out.println("time's up : " + timeLeft);
+		    		timer.cancel();
+		    	}
+		    }
+		    
+		};
+
+		timer.schedule(task, 0, 1000);
+	//	timer.cancel();
+        
+	}
 	public static TreasureHunt getInstance() {
-		if (treasureHunt == null) {
-			treasureHunt = new TreasureHunt() ;
-			treasureHunt.init() ;
 			return treasureHunt ;
-		}
-		else {
-			return treasureHunt ;
-		}
 	}
-	
-	private void init() {
+	public Map<String, Object> getScoreMap(){
+        return map;
 	}
-	
-	
+	public ArrayList getWinnerList(){
+		return winnerList;
+	}
 	
 }
