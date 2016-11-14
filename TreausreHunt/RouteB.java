6206a8127042a06e5c60483733bbb73be2bfe240
Adithya KLN <adithyakarlapalem@gmail.com>
2016-11-13 21:28:01 -0800

    Updated Route B for Fuel and Next Island
    Updating Fuel Needed for Route B.

--- TreausreHunt/RouteB.java
+++ TreausreHunt/RouteB.java
@@ -1,24 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class RouteB here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.*;
 public class RouteB extends Route
 {
     private Island islandRouteBelongsTo;
+    private int fuelNeeded = 0;
     
     public void act() 
     {
        if(Greenfoot.mouseClicked(this)){
            if(this.islandRouteBelongsTo.getHasShip()){
                TreasureHuntWorld treasureHuntWorld = (TreasureHuntWorld) getWorld();
-               treasureHuntWorld.getShip().moveToIsland(islandRouteBelongsTo.getNextIslandB());
+               treasureHuntWorld.getShip().moveToIsland(islandRouteBelongsTo.getNextIslandB(),this);
                this.islandRouteBelongsTo.setHasShip(false);
-            }
-        }
+           }
+       }
     } 
     
     public void setIslandRouteBelongsTo(Island islandRouteBelongsTo){
@@ -28,4 +23,12 @@ public class RouteB extends Route
     public Island getIslandRouteBelongsTo(){
         return this.islandRouteBelongsTo;
     }  
+    
+    public void setFuelNeeded(int fuelNeeded){
+        this.fuelNeeded = fuelNeeded;
+    }
+    
+    public int getFuelNeeded(){
+        return this.fuelNeeded;
+    }
 }
